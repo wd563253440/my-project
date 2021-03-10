059bebb805217d8876e7f46b74536b8e26e368f6
wd <18986935527@163.com>
2021-03-10 22:20:47 +0800

    实现一级菜单

--- src/renderer/components/navTab/navTab.vue
+++ src/renderer/components/navTab/navTab.vue
@@ -3,11 +3,20 @@
     <div class="nav-top-left">
 
     </div>
-    <div class="nav-top">
-
+    <div class="nav-top" @click="level1_init">
+      {{this.level1}}
     </div>
     <div class="nav-left">
-      
+      <div class="nav-level" v-for='(item,index) in nav_length' :key="item.index">
+        <a href="#">
+          <li>
+            <div class='nav-level2' v-if="level1[index].length > 1">
+              aa
+            </div>
+            <span>{{level1[index][0]}}</span>
+          </li>
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -18,19 +27,40 @@ export default {
   name: 'navTab',
   data () {
     return {
-      navTabData: ''
+      nav_length: 0,
+      level1: ''
     }
   },
   mounted () {
-    this.navTabData = navTabData
+    this.nav_length = navTabData.navTabData2.length
+    this.level1 = navTabData.navTabData2
+  },
+  methods: {
+    level1_init () {
+      console.log(this.level1.length)
+    }
   }
 }
 </script>
 <style>
+.nav-level2 {
+  display: none;
+}
+.nav-level {
+  height: 40px;
+	line-height: 40px;
+	font-size: 14px;
+	color: #303133;
+	padding: 0 20px;
+	list-style: none;
+	cursor: pointer;
+	position: relative;
+}
 .nav-top-left {
   width: 200px !important;
   height: 40px;
   background-color: lawngreen;
+  overflow: hidden;
 }
 .nav-top {
   height: 40px !important;
@@ -47,7 +77,7 @@ export default {
   background-color: #304156;
   height: 100%;
   position: fixed;
-  font-size: 0;
+  font-size: 12px;
   top: 0;
   bottom: 0;
   left: 0;
