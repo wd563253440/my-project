3d60b5b3dab7cfd293a933e0df2440dcc80b75ce
wd <18986935527@163.com>
2021-03-10 21:35:13 +0800

    整体的布局排版,左侧导航,图标区域,顶部导航

--- src/renderer/components/navTab/navTab.vue
+++ src/renderer/components/navTab/navTab.vue
@@ -1,26 +1,59 @@
 <template>
-  <div class="nav-div">
-    
+  <div>
+    <div class="nav-top-left">
+
+    </div>
+    <div class="nav-top">
+
+    </div>
+    <div class="nav-left">
+      
+    </div>
   </div>
 </template>
 <script>
-  export default {
-    name: 'navTab'
+import navTabData from '../../../../test/navTab/navTabData'
+console.log(navTabData.navTabData)
+export default {
+  name: 'navTab',
+  data () {
+    return {
+      navTabData: ''
+    }
+  },
+  mounted () {
+    this.navTabData = navTabData
   }
+}
 </script>
 <style>
-  .nav-div{
-    -webkit-transition: width .28s;
-    transition: width .28s;
-    width: 200px!important;
-    background-color: #304156;
-    height: 100%;
-    position: fixed;
-    font-size: 0;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 1001;
-    overflow: hidden
-  }
+.nav-top-left {
+  width: 200px !important;
+  height: 40px;
+  background-color: lawngreen;
+}
+.nav-top {
+  height: 40px !important;
+  background-color:grey;
+  z-index: 1001;
+  margin-left: 200px;
+  margin-top: -40px;
+  overflow: hidden;
+}
+.nav-left {
+  -webkit-transition: width 0.28s;
+  transition: width 0.28s;
+  width: 200px !important;
+  background-color: #304156;
+  height: 100%;
+  position: fixed;
+  font-size: 0;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1001;
+  overflow: hidden;
+  margin-top: 40px;
+}
+
 </style>
--- test/navTab/navTabData.js
+++ test/navTab/navTabData.js
@@ -1,5 +1,36 @@
-let navTab = `
-    
-`
+let navTabData = {
+  'level1': {
+    'level2': {
+      'level3': '第三级菜单'
+    }
 
-export default navTab
+  }
+}
+
+let navTabData2 = [
+  ['菜单一'],
+  [
+    '菜单一', [
+      '菜单二',
+      '菜单二',
+      '菜单二', [
+        '菜单三',
+        '菜单三'
+      ]
+    ]
+  ],
+  [
+    '菜单一', [
+      '菜单二',
+      '菜单二',
+      '菜单二', [
+        '菜单三',
+        '菜单三'
+      ]
+    ]
+  ]
+]
+export default {
+  navTabData2,
+  navTabData
+}
